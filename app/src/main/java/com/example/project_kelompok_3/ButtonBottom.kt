df044de606fe722dc987390bdb2c67f95b28a9f0
Rafie <rafieashraf716@gmail.com>
2025-11-05 09:22:03 +0700

    Wasn't expecting this one

--- app/src/main/java/com/example/project_kelompok_3/ButtonBottom.kt
+++ app/src/main/java/com/example/project_kelompok_3/ButtonBottom.kt
@@ -3,15 +3,11 @@ package com.example.project_kelompok_3
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.Send
-import androidx.compose.material.icons.filled.Send
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Search
